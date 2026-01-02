be4cf1e00928fd0374b6b899d3c8770f4870f958
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2026-01-03 01:07:57 +0530

    fixed cors issue

--- src/index.js
+++ src/index.js
@@ -11,8 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  })
+);
+
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
